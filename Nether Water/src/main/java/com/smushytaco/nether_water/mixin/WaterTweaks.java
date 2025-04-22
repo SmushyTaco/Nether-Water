8bfd2240d64d8d84f1b534fdd855d8351a7fe224
Nikan Radan <github.com@alias.nikanradan.com>
2025-04-22 14:19:07 -0700

    1.21.5

--- Nether Water/src/main/java/com/smushytaco/nether_water/mixin/WaterTweaks.java
+++ Nether Water/src/main/java/com/smushytaco/nether_water/mixin/WaterTweaks.java
@@ -2,7 +2,7 @@ package com.smushytaco.nether_water.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.smushytaco.nether_water.NetherWater;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.BucketItem;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
@@ -14,5 +14,5 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(BucketItem.class)
 public abstract class WaterTweaks {
     @WrapOperation(method = "placeFluid", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/dimension/DimensionType;ultrawarm()Z"))
-    private boolean hookPlaceFluid(DimensionType instance, Operation<Boolean> original, @Nullable PlayerEntity player, World world, BlockPos pos, @Nullable BlockHitResult hitResult) { return original.call(instance) && !NetherWater.INSTANCE.canHaveWater(pos.getY()); }
+    private boolean hookPlaceFluid(DimensionType instance, Operation<Boolean> original, @Nullable LivingEntity user, World world, BlockPos pos, @Nullable BlockHitResult hitResult) { return original.call(instance) && !NetherWater.INSTANCE.canHaveWater(pos.getY()); }
 }
