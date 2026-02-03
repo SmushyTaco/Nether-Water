5d356745b105d27849e1ec078192e4052c9bac83
Nikan Radan <github.com@alias.nikanradan.com>
2026-02-03 00:38:59 -0800

    1.21.11

--- Nether Water/src/main/java/com/smushytaco/nether_water/mixin/WaterTweaks.java
+++ Nether Water/src/main/java/com/smushytaco/nether_water/mixin/WaterTweaks.java
@@ -1,18 +1,16 @@
 package com.smushytaco.nether_water.mixin;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.smushytaco.nether_water.NetherWater;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.phys.BlockHitResult;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 @Mixin(BucketItem.class)
 public abstract class WaterTweaks {
-    @WrapOperation(method = "emptyContents", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/dimension/DimensionType;ultraWarm()Z"))
-    private boolean hookPlaceFluid(DimensionType instance, Operation<Boolean> original, @Nullable LivingEntity user, Level world, BlockPos pos, @Nullable BlockHitResult hitResult) { return original.call(instance) && !NetherWater.INSTANCE.canHaveWater(pos.getY()); }
+    @ModifyExpressionValue(method = "emptyContents", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/material/Fluid;is(Lnet/minecraft/tags/TagKey;)Z"))
+    private boolean hookPlaceFluid(boolean original, @Nullable LivingEntity livingEntity, Level level, BlockPos blockPos, @Nullable BlockHitResult blockHitResult) { return original && !NetherWater.INSTANCE.canHaveWater(blockPos.getY()); }
 }
